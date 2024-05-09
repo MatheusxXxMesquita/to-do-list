c76c56c37b15302c74ba4677bab07fc79763cf62
MatheuxXxMesquita <saydox45@gmail.com>
2024-05-09 12:11:34 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import "primereact/resources/themes/lara-light-amber/theme.css";
+import "primeicons/primeflex.css";
+import "primeflex/primeflex.css";
+
+import {Button} from "primereact/button"
+
+
+const App = () =>{
+  return (
+    <> 
+    
+    </>
+  );
+}
+
+export default App;
